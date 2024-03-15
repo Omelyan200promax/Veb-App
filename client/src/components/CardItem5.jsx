0ec8dc1745924bc22b3ead24b9e04b2aab2464b6
Omelyan200promax <annabrest2008@gmail.com>
2024-03-15 19:25:39 +0300

    Add info and img to main page, font

---
+++ client/src/components/CardItem5.jsx
@@ -0,0 +1,27 @@
+import "../App.css";
+import {Flex,Heading,Button,Link, Image, Box} from '@chakra-ui/react';
+import {Card,CardHeading,CardBody,CardFooter} from '@chakra-ui/react';
+import Obl  from '../components/Obl.webp'
+
+const CardItem5 =() =>{
+    return(
+        <Card width ={'280px'} height={'300px'} boxShadow={'-1px 2px 14px -3px  black'} borderRadius ={'8px'} >
+            
+            <Flex justify={'center'} align={'center'} width ={'100%'} height ={'190px'}><Image
+                src ={Obl}
+                width ={'47%'}
+                height={'88%'}
+            ></Image></Flex>
+            <Flex width={'100%'} he justify={'center'} align={'center'} ><p text-align={'center'}><b>Обломов</b></p></Flex>
+            <Flex width={'100%'} he justify={'center'} align={'center'} ><p text-align={'center'}>И. А. Гончаров </p></Flex>
+
+            <Flex width ={'100%'} justify={'space-around'} align={'center'}>
+               
+                <Flex width ={'50%'} >Цена: 20 руб.</Flex>
+                <Button width ={'30%'} color={'black'} background='rgb(225, 185, 133)'  >Заказать</Button>
+            </Flex>
+
+        </Card>
+    )
+}
+export default CardItem5; 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,20 +1,65 @@
 import '../App.css';
 import NavBar from '../components/NavBar';
-import {Flex,Heading,Button,Link, Image, Box} from '@chakra-ui/react';
+import {Flex,Heading,Button,Link, Image, Box, Text} from '@chakra-ui/react';
 import Footer from  '../components/Footer';
 import colors from '../utils/colors';
 import ToTopButton from '../components/ToTopButton';
+import osnova from '../pages/osnova.jpg';
+import books from '../pages/books.jpg';
+import books2 from '../pages/books2.jpg'
+import books3 from '../pages/books3.jpg'
+
 
 const HomePage =()=>{
     
     const {coffe,darkCoffe,brightBalck, mintWhite} = colors();
     return(
-        <Box height={'100vh'} >
+        <Box height={'100vh' } >
             <>
                 <NavBar />
                 {/* СДЕЛАТЬ ПЕРЕМЕННЫЕ С КАРТИНКАМИ КАК В CARDINEM */}
-                <img src = 'osnova.jpg' alt = 'книги'></img>
-                <img src = 'books.jpg' alt = 'книги'></img>
+                {/* <Image> src= {osnova}</Image>
+                <Image> src= {books}</Image> */}
+               
+                <Image
+                    mt ={'15vh'}
+                   
+                    src ={osnova}
+                    width ={'50%'}
+                    height={'50%'}
+                ></Image>
+                <Flex justify ={'center'} align = {'center'} width = {'270px'} height = {'230px'} position = {'absolute'} mt ={'-330px'} ml ={'1400px'} boxShadow={'12px 10px 15px -2px  rgb(225, 185, 133) '}><Text  fontSize ='25px' as ='b'>Более 2000 книг на любой вкус и цвет 📚<br/> <br/>Выгодные акции еженедельно 💯 </Text></Flex>
+               
+
+                <Image
+                     position = {'absolute'}
+                     right ={'0'}
+                    mt ={'13vh'}
+                    src ={books}
+                    width ={'75%'}
+                    height={'60%'}
+                ></Image>
+
+                <Box width = {'300px'}  position = {'absolute'} ml={'90px'} mt = {'300px'} boxShadow={'-12px 10px 15px -2px  rgb(225, 185, 133) '} ><Text fontSize ='25px' as ='b'>Книги любых жанров. Учебные пособия для всех возрастов. Редкие экземпляры. Найдите то, что нужно вам!  </Text></Box>
+                 <Image
+                   
+                    mt ={'86vh'}
+                    src ={books2}
+                    width ={'55%'}
+                    height={'50%'}
+                ></Image>
+                <Box width = {'310px'} boxShadow={'12px 10px 15px -2px  rgb(225, 185, 133) '} position = {'absolute'}   mt ={'-300px'} ml ={'1400px'}><Text  fontSize ='25px' as ='b'>Наш адрес:<br/> ул. Советская 54, г. Брест </Text></Box>
+                 <Image
+                    position = {'absolute'}
+                    right ={'0'}
+                    mt = {'130px'}
+                    src ={books3}
+                    width ={'75%'}
+                    height={'60%'}
+                ></Image>
+                <Box width = {'350px'}  boxShadow={'-12px 10px 15px -2px  rgb(225, 185, 133) '} position = {'absolute'} ml={'100px'} mt = {'340px'} ><Text fontSize ='24px' as ='b'>Заглядывай в "Книжного Червя" почаще и проводи время с пользой ! 😉 </Text></Box>
+                
+                
                 <ToTopButton/>
             
                 <Footer />
